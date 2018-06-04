f712145edc2f6ce3924e9059e06dae130498a726
seeways <1876665310@qq.com>
2018-06-04 17:34:12 +0800

    fix bug

--- DjangoEduSysDemo/settings.py
+++ DjangoEduSysDemo/settings.py
@@ -133,7 +133,7 @@ AUTH_USER_MODEL = 'users.UserProfile'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
